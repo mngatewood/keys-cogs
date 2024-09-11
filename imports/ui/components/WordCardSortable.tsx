c47a87561120d6f7dcb01bf3d832c3bc2f16ed5f
Michael Gatewood <michael@coincloudnv.com>
2024-09-10 23:00:00 -0700

    Add sortable word-card (WIP) - Rename WordCard to WordCardDraggable and WordCardSortable

--- imports/ui/components/WordCardSortable.tsx
+++ imports/ui/components/WordCardSortable.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import { useDraggable } from '@dnd-kit/core';
-import {CSS} from '@dnd-kit/utilities';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import type { Card } from './Game';
 
-export const WordCard = (card: Card) => {
+export const WordCardSortable = (card: Card) => {
 	const keywords = card.words;
 
-	const { attributes, listeners, setNodeRef, transform } = useDraggable({
-		id: card._id,
-	});
+	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: card._id });
 
 	const style = {
-		transform: CSS.Translate.toString(transform),
-	}
+		transform: CSS.Transform.toString(transform),
+		transition,
+	};
 
 	return (
 		<div className='word-card' id={card._id} ref={setNodeRef} style={style} {...listeners} {...attributes}>
