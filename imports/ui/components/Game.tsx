45f431f47188716820b7ebaed76630a7951b155e
Michael Gatewood <michael@coincloudnv.com>
2024-09-13 17:35:58 -0700

    - Add buttons to remove card from cog - Add button to remove all cards from cog - Refactor card position logic - Add data validation on state update

--- imports/ui/components/Game.tsx
+++ imports/ui/components/Game.tsx
@@ -36,7 +36,6 @@ export const Game = () => {
 		const loadData = () => {
 			const playCardsData = cardsData.filter((card) => card.position === 5);
 			const cogCardsData = cardsData.filter((card) => card.position !== 5);
-			console.log(playCardsData, cogCardsData);
 
 			const playCardElements = playCardsData.map((card) => {
 				return <WordCardDraggable key={card._id} {...card} />;
@@ -44,9 +43,11 @@ export const Game = () => {
 			setPlayCards(sortByPosition(playCardElements));
 
 			const cogCardElements = cogCardsData.map((card) => {
-				return <WordCardSortable key={card._id} {...card} />;
+				return <WordCardSortable key={card._id} card={card} removeCard={handleRemoveCard}/>;
 			});
 			setCogCards(sortByPosition(cogCardElements));
+
+			validateCardsState();
 		};
 		loadData();
 	}, [cardsData]);
@@ -90,70 +91,138 @@ export const Game = () => {
 	}
 
 	const moveCard = (cardData: Card, origin: number, destination: number) => {
-		let updatedCard, updatedPlayCards, updatedCogCards;
-		const cardToShiftData = cardsData.find((card) => card.position === destination);
-		updatedCard = <WordCardSortable key={cardData._id} {...cardData} position={destination} />;
-		cardData.position = destination;
+		// console.log(cardData, origin, destination);
+		let cards = cardsData;
+		const cardToShiftData = cards.find((card) => card.position === destination);
+		let placeholderCardData = cards.find((card) => {
+			return cogContainers.includes(parseInt(card._id)) && card.position === destination;
+		});
 
-		if(origin === 5  && cardToShiftData && cogContainers.includes(parseInt(cardToShiftData._id))) { 
+		if (origin === 5 && placeholderCardData) { 
 		// moving from play to empty cog space
 
-			updatedPlayCards = playCards.filter((card) => card.key !== updatedCard.key);
-			updatedCogCards = [...cogCards.filter((card) => card.key !== cardToShiftData?._id), updatedCard];
-
-		} else if(origin === 5 && cardToShiftData) { 
+			console.log("moving from play to empty cog space");
+			placeholderCardData.position = 5;
+			cardData.position = destination;
+		
+		} else if(origin === 5 && !placeholderCardData) { 
 		// moving from play to non-empty cog space
 
+			console.log("moving from play to non-empty cog space");
 			const cogPlaceholders = cogCards.filter((card) => ["1", "2", "3", "4"].includes(card?.key as string));
 
 			if(cogPlaceholders.length > 0) { 
 			// shift to empty space if possible
 
-				const emptyCogSpaces = cogPlaceholders.map((card) => cardsData.find((cardData) => cardData._id === card.key)?.position);
-				let cardToRemoveFromCog;
+				console.log("empty space found, shifting card to shift to empty space");
+				const cogPlaceholdersData = cogPlaceholders.map((card) => cards.find((cardData) => cardData._id === card.key));
+				const emptyCogSpaces = cogPlaceholdersData.map((placeholderData) => placeholderData?.position);
 
 				if(emptyCogSpaces.includes(destination + 1)) { 
 				// shift to next space if empty
 
-					const cardToRemoveFromCogData = cardsData.find((card) => card.position === destination + 1);
-					cardToRemoveFromCog = cogCards.find((card) => card.key === cardToRemoveFromCogData?._id);
-					cardToShiftData.position = destination + 1;
-
-				} else { 
+					console.log("shifting card to next space")
+					placeholderCardData = cards.find((card) => {
+						return cogContainers.includes(parseInt(card._id)) && card.position === destination + 1;
+					});
+					if (placeholderCardData) placeholderCardData.position = 5;
+					if (cardToShiftData) cardToShiftData.position = destination + 1;
+					cardData.position = destination;
+					
+				} else if (emptyCogSpaces[0] !== undefined) { 
 				// shift to first empty space for now
 
-					const placeholderToShift = cardsData.find((card) => card._id === cogPlaceholders[0]?.key);
-					const cardToRemoveFromCogData = cardsData.find((card) => card._id === cogPlaceholders[0].key);
-					cardToRemoveFromCog = cogCards.find((card) => card.key === cardToRemoveFromCogData?._id);
-					cardToShiftData.position = placeholderToShift ? placeholderToShift.position : cardToShiftData.position;
+					console.log("shifting card to first empty space")
+					placeholderCardData = cardsData.find((card) => card._id === emptyCogSpaces[0]?.toString());
+					if (placeholderCardData) placeholderCardData.position = 5;
+					if (cardToShiftData) cardToShiftData.position = emptyCogSpaces[0];
+					cardData.position = destination;
 				}
-				updatedPlayCards = [...playCards.filter((card) => card.key !== updatedCard.key)];
-				updatedCogCards = [...cogCards.filter((card) => card.key !== cardToRemoveFromCog?.key), updatedCard];
 
 			} else { 
 			// shift to play space if no empty spaces
 
-				cardToShiftData.position = 5;
-				const cardToShift = <WordCardDraggable key={cardToShiftData._id} {...cardToShiftData} />;
-				updatedPlayCards = [...playCards.filter((card) => card.key !== updatedCard?.key), cardToShift];
-				updatedCogCards = [...cogCards.filter((card) => card.key !== cardToShiftData?._id), updatedCard]; 
+				console.log("shifting card to play space")
+				if (cardToShiftData) cardToShiftData.position = 5;
+				cardData.position = destination;
+
 			}
+
+		} else if (destination === 5) {
+		// moving from cog to play
+
+			console.log("moving from cog to play")
+			placeholderCardData = cardsData.find((card) => card._id === origin.toString());
+			if (placeholderCardData) placeholderCardData.position = origin;
+			cardData.position = destination;
+
 
 		} else { 
 		// moving from cog to cog
 
-			if(cardToShiftData) {
-				cardToShiftData.position = origin;
-			}
+			console.log("moving from cog to cog")
+			if(cardToShiftData) cardToShiftData.position = origin;
+			cardData.position = destination;
 
-			updatedPlayCards = playCards;
-			updatedCogCards = cogCards;
 		}
+
+		const updatedPlayCardData = cardsData.filter((cardData) => cardData.position === 5);;
+		const updatedCogCardData = cardsData.filter((cardData) => cogContainers.includes(cardData.position));
+		let updatedPlayCards = updatedPlayCardData.map((cardData) => <WordCardDraggable key={cardData._id} {...cardData} />);
+		const updatedCogCards = updatedCogCardData.map((cardData) => <WordCardSortable key={cardData._id} card={cardData} removeCard={handleRemoveCard}/>);
 
 		// strip out any placeholder cards
 		updatedPlayCards = updatedPlayCards.length ? updatedPlayCards.filter((card) => card && !["1","2","3","4"].includes(card.key || "")) : updatedPlayCards
 
 		setPlayCards(sortByPosition(updatedPlayCards.filter((card) => card !== undefined)));
+		setCogCards(sortByPosition(updatedCogCards));
+		setCardsData(cardsData);
+	}
+
+	const validateCardsState = () => {
+		if (!isPlaying) return;
+		const placeholdersData = cardsData.filter((card) => cogContainers.includes(parseInt(card._id)));
+		const placeholderIds = placeholdersData.map((card) => card._id);
+		const wordCardsData = cardsData.filter((card) => !placeholderIds.includes(card._id));
+		const updatedCogCardsOnlyData = wordCardsData.filter((card) => {
+			return cogContainers.includes(card.position);
+		});
+		const occupiedCogSpaces = updatedCogCardsOnlyData.map((card) => card.position);
+		const emptyCogSpaces = cogContainers.filter((space) => !occupiedCogSpaces.includes(space));
+		let updatedCogCardsData: Card[] = [];
+		emptyCogSpaces.forEach((space) => {
+			const placeholder = placeholdersData.find((card) => card._id === space.toString());
+			if (placeholder) {
+				updatedCogCardsData.push(placeholdersData.find((card) => card._id === space.toString()) as Card);
+			}
+		});
+		updatedCogCardsData.push(...updatedCogCardsOnlyData)
+		let updatedPlayCardsData = cardsData.filter((card) => card.position === 5 && !placeholdersData.includes(card));
+		updatedPlayCardsData.forEach((card) => {
+			card.position = 5;
+		})
+
+		if (placeholdersData.length !== 4 
+			|| updatedCogCardsData.length !== 4 
+			|| updatedCogCardsOnlyData.length + updatedPlayCardsData.length !== 5
+			|| cardsData.length !== 9) {
+				updatedCogCardsData.length !== 4 && console.log("invalid number of cog cards in state");
+				updatedCogCardsOnlyData.length + updatedPlayCardsData.length !== 5 && console.log("invalid number of non-placeholder cards in state");
+				cardsData.length !== 9 && console.log("invalid total number of cards in state");
+				console.log("invalid cards state");
+				console.log("placeholdersData", placeholdersData);
+				console.log("updatedCogCardsOnlyData", updatedCogCardsOnlyData);
+				console.log("updatedPlayCardsData", updatedPlayCardsData);
+				console.log("cardsData", cardsData);
+				console.log("updatedCogCardsData", updatedCogCardsData);
+				console.log("emptyCogSpaces", emptyCogSpaces);
+				console.log("occupiedCogSpaces", occupiedCogSpaces);
+			}
+
+		const updatedCogCards = updatedCogCardsData.map((cardData) => <WordCardSortable key={cardData._id} card={cardData} removeCard={handleRemoveCard} />);
+		const updatedPlayCards = updatedPlayCardsData.map((cardData) => <WordCardDraggable key={cardData._id} {...cardData} />); 
+
+		setPlayCards(sortByPosition(updatedPlayCards));
 		setCogCards(sortByPosition(updatedCogCards));
 	}
 
@@ -170,10 +239,29 @@ export const Game = () => {
 		const movedCardData = cardsData.find((card) => card._id === event.active.id) as Card;
 		const origin = movedCardData.position;
 		if(event.over?.id) {			
-			const destination = cardsData.find((card) => card._id === event.over.id)?.position || 0;
+			const destination = cardsData.find((card) => card._id === event.over.id)?.position || 5;
 			moveCard(movedCardData, origin, destination);
 		}
 		document.getElementById(movedCardData._id)?.classList.remove("z-top");
+	}
+
+	const handleResetCards = () => {
+		const updatedCards = cardsData.map((card) => {
+			if(["1", "2", "3", "4"].includes(card._id)) {
+				card.position = parseInt(card._id);
+			} else {
+				card.position = 5;
+			}
+			return card;
+		});
+		setCardsData(updatedCards);
+	}
+
+	const handleRemoveCard = (cardId: string) => {
+		const updatedCard = cardsData.find((card) => card._id === cardId) as Card;
+		if (updatedCard) {
+			moveCard(updatedCard, updatedCard.position, 5);
+		}
 	}
 
 	return (
@@ -186,7 +274,7 @@ export const Game = () => {
 			}
 			{ isPlaying &&
 				<div className="cog-container">
-					<CogKeys updateKeys={handleKeyUpdate} keys={keys}/>
+					<CogKeys updateKeys={handleKeyUpdate} resetCards={handleResetCards}keys={keys}/>
 					<SortableContext items={cogCards.map((card) => card.key || "")} strategy={rectSwappingStrategy} >
 						<div className="droppable-container">
 							{ cogCards.map((card) => (
