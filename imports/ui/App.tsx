78af6f590e905309c47702ecd9eaa165f0a6b697
Michael Gatewood <michael@coincloudnv.com>
2024-09-17 12:18:13 -0700

    Render login component if no user

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Game } from './components/Game';
+import { Login } from './components/Login';
 
 export const App = () => {
+	const user = useTracker(() => Meteor.user());
+
 	return (
-		<div className='app'>
-			<Game />
+		<div>
+			{user ? <Game /> : <Login />}
 		</div>
-	)
+	);
 };
