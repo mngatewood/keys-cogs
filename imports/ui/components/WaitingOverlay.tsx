b6945fee44801a42a62a7b1f52f9043d20d8e587
Michael Gatewood <michael@coincloudnv.com>
2024-09-27 09:58:09 -0700

    Disable start next round button until all players have saved

--- imports/ui/components/WaitingOverlay.tsx
+++ imports/ui/components/WaitingOverlay.tsx
@@ -17,7 +17,7 @@ export const WaitingOverlay = ({allPlayersReady, advanceRound}: WaitingOverlayPr
 				<h1 className="text-2xl font-bold text-center mb-4">
 					{allPlayersReady() ? "All players ready" : "Waiting for other players..."}
 				</h1>
-				<button onClick={handleAdvanceRound} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 disabled:bg-gray-200 disabled:text-gray-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+				<button disabled={!allPlayersReady()} onClick={handleAdvanceRound} className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 disabled:bg-gray-200 disabled:text-gray-400 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
 					Start Next Round
 				</button>
 			</div>
