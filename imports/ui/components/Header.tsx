127dc623dc20610fae03efbb538bc997938c6cfc
Michael Gatewood <michael@coincloudnv.com>
2024-10-02 12:24:53 -0700

    - Update Header layout - Add play icon to Header

--- imports/ui/components/Header.tsx
+++ imports/ui/components/Header.tsx
@@ -4,33 +4,43 @@ import { Link } from 'react-router-dom';
 
 export const Header = ({ user }: { user: Meteor.User }) => {
 	return (
-		<header className="navbar">
-			<div>
-				<Link to="/">
-					<img className='header-img' src='/home-icon.png' />
-				</Link>
-			</div>
-			<div className="app-title">
-				<div>Keys</div>
-				<div>
-					<img className='header-img title-cog-img' src='/header-cog-icon.png' />
+		<header>
+			<div className="navbar">
+				<div className="header-icons-container home-icon-container">
+					<Link to="/">
+						<img className='header-img' src='/home-icon.png' />
+					</Link>
 				</div>
-				<div>Cogs</div>
-			</div>
-			<div>
-				{!user && 
+				<div className="app-title">
+					<div>Keys</div>
 					<div>
-						<Link to="/login">
-							<img className='header-img' src='/account-icon.png' />
-						</Link>
-					</div>}
-				{user && 
-					<div>
-						<Link to="/account">
-							<img className='header-img' src='/account-icon.png' />
-						</Link>
+						<img className='header-img title-cog-img' src='/header-cog-icon.png' />
 					</div>
-				}
+					<div>Cogs</div>
+				</div>
+				<div className="header-icons-container">
+					{!user && 
+						// <div>
+							<Link to="/login">
+								<img className='header-img' src='/account-icon.png' />
+							</Link>
+						// </div>	
+					}
+					{user && 
+						<>
+							{/* <div> */}
+								<Link to="/play">
+									<img className='header-img mr-4' src='/play-icon.png' />
+								</Link>
+							{/* </div> */}
+							{/* <div> */}
+								<Link to="/account">
+									<img className='header-img' src='/account-icon.png' />
+								</Link>
+							{/* </div> */}
+						</>
+					}
+				</div>
 			</div>
 		</header>
 	);
