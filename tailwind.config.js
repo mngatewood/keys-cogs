61a28686494331be3aee9daaec5498ea3aa17214
Michael Gatewood <michael@coincloudnv.com>
2024-10-02 11:54:24 -0700

    Add theme colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./imports/ui/**/*.{js,jsx,ts,tsx}",
-    "./client/*.html",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	/** @type {import('tailwindcss').Config} */
+	module.exports = {
+		content: [
+			"./imports/ui/**/*.{js,jsx,ts,tsx}",
+			"./client/*.html",
+		],
+		theme: {
+			extend: {
+				colors: {
+					beige: {
+						1: '#f3f0d8'
+					},
+					rosegold: {
+						1: '#b97171'
+					},
+					yellow: {
+						1: '#f6cc08',
+						2: '#e9ba14'
+					},
+					blue: {
+						1: '#719cc6',
+						2: '#5a8ca8',
+					},
+				}
+			}
+		},
+		plugins: [],
+	}
 
