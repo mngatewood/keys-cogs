4c64cf663eebb17b8f7e9d308509b90f6c5d3bfc
Michael Gatewood <michael@coincloudnv.com>
2024-10-04 16:38:09 -0700

    - Fix flex properties to allow scroll on landscape - Update button styles to match theme

--- imports/ui/components/Menu.tsx
+++ imports/ui/components/Menu.tsx
@@ -23,17 +23,17 @@ export const Menu: React.FC<MenuProps> = ({joinGame, showGames}) => {
 
 	return (
 		<>
-			<div className="home-container">
-				<>
+			<div className="flex items-center justify-center overflow-scroll w-full h-full">
+				<div className="bg-beige-1 shadow-lg border border-gray-300 rounded-lg px-8 py-6 m-auto max-w-md w-5/6">
 					<h1 className="text-2xl font-bold text-center mb-4">Ready to begin?</h1>
 					<h3 className="text-lg font-bold text-center mb-4">Click "Host a Game" to start a new game or "Join a Game" to join an existing game.</h3>
-					<button className="button w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-1 disabled:bg-gray-200 disabled:text-gray-400 hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-1" onClick={handleHostGame}>
+					<button className="button w-full flex justify-center my-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-1 disabled:bg-rosegold-2 disabled:text-gray-400 hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-1" onClick={handleHostGame}>
 						Host a Game
 					</button>
-					<button className="button w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-1 disabled:bg-gray-200 disabled:text-gray-400 hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-1" onClick={handleListGames}>
+					<button className="button w-full flex justify-center my-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-1 disabled:bg-rosegold-2 disabled:text-gray-400 hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-1" onClick={handleListGames}>
 						Join a Game
 					</button>
-				</>
+				</div>
 			</div>
 		</>
 	);
