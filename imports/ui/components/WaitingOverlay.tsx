32807fb8cf98b1146164ff6d3e8dfd02f0efbd1c
Michael Gatewood <michael@coincloudnv.com>
2024-10-03 19:02:24 -0700

    Update overlay and icons colors to match theme

--- imports/ui/components/WaitingOverlay.tsx
+++ imports/ui/components/WaitingOverlay.tsx
@@ -14,7 +14,7 @@ export const WaitingOverlay = ({allPlayersReady, advanceRound, roundResults}: Wa
 	}
 
 	return (
-		<div className="h-screen w-full fixed top-0 left-0 flex items-center justify-center z-50 bg-purple-500 bg-opacity-50 ">
+		<div className="h-screen w-full fixed top-0 left-0 flex items-center justify-center z-50 bg-blue-1 bg-opacity-50 ">
 			<div className="bg-white shadow-lg border border-gray-300 rounded-lg px-8 py-6 w-5/6 max-w-md">
 				<h1 className="text-2xl font-bold text-center mb-4">
 					{ allPlayersReady() ? "All players ready" : roundResults.message }
