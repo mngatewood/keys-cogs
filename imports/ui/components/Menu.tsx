3581438267a4ea5e99273c29e7df90027b2ab23f
Michael Gatewood <michael@coincloudnv.com>
2024-10-04 19:35:23 -0700

    Add game overview and figures

--- imports/ui/components/Menu.tsx
+++ imports/ui/components/Menu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
+import { getDemo } from '/imports/helpers/utils';
 
 interface MenuProps {
 	joinGame: Function,
@@ -21,6 +22,19 @@ export const Menu: React.FC<MenuProps> = ({joinGame, showGames}) => {
 		showGames();
 	}
 
+	const playDemo = async () => {
+		const demo = await getDemo();
+		Meteor.loginWithPassword(demo.demoPlayer.username, demo.demoPlayer.password, (error) => {
+			if (error) {
+				console.log(error);
+			} else if (demo.demoGame.players.map((player: { _id: string }) => player._id).includes(demo.demoPlayer._id)) {
+				localStorage.setItem("gameId", demo.demoGame._id);
+				// navigate("/play");
+			}
+		})
+	}
+
+
 	return (
 		<>
 			<div className="flex items-center justify-center overflow-scroll w-full h-full">
@@ -32,6 +46,9 @@ export const Menu: React.FC<MenuProps> = ({joinGame, showGames}) => {
 					</button>
 					<button className="button w-full flex justify-center my-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-1 disabled:bg-rosegold-2 disabled:text-gray-400 hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-1" onClick={handleListGames}>
 						Join a Game
+					</button>
+					<button className="button w-full flex justify-center my-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-1 disabled:bg-rosegold-2 disabled:text-gray-400 hover:bg-blue-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-1" onClick={playDemo}>
+						Play the Demo
 					</button>
 				</div>
 			</div>
